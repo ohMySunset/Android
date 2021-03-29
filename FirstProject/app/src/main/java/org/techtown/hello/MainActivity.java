43ec33e5183b22f3b2971bc29fbfd3a64b9e3def
ohMySunset <znfnznfnakqj@naver.com>
2021-03-29 20:13:27 +0900

    Update : add ButtonEvent(Link, Call)

--- FirstProject/app/src/main/java/org/techtown/hello/MainActivity.java
+++ FirstProject/app/src/main/java/org/techtown/hello/MainActivity.java
@@ -1,5 +1,7 @@
 package org.techtown.hello;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -16,5 +18,15 @@ public class MainActivity extends AppCompatActivity {
 
     public void onButton1Clicked(View v){
         Toast.makeText(this.getApplicationContext(), "확인1 버튼이 눌렸어요.", Toast.LENGTH_LONG).show();
+    }
+
+    public void onButton2Clicked(View v){
+        Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://m.naver.com"));
+        startActivity(myIntent);
+    }
+
+    public void onButton3Clicked(View v){
+        Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:010-1000-1000"));
+        startActivity(myIntent);
     }
 }
